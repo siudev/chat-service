077ab2f74e65db70590aa760752fe23a265c7a87
Reid Lee <reid@socialinus.com>
2013-12-23 17:41:06 +0900

    express api를 이용한 서버 기본 구조

---
+++ server/chat-server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.configure(function() {
+	app.use(express.bodyParser());
+});
+
+app.configure('development', function() {
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function() {
+	app.use(express.errorHandler());
+});
+
+/*
+app.get("/crossdomain.xml", onCrossDomainHandler)
+function onCrossDomainHandler(req, res) {
+	var xml = '<?xml version="1.0"?>\n';
+	xml += '<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">\n';
+	xml += '<cross-domain-policy>\n';
+	xml += '<allow-http-request-headers-from domain="*" headers="*" />\n';
+	xml += '<allow-access-from domain="*" />\n';
+	xml += '</cross-domain-policy>\n';
+
+	req.setEncoding('utf8');
+	res.writeHead(200, {'Content-Type': 'text/xml'});
+	res.end(xml);
+}
+*/
+
+app.get('/chat', function(req, res) {
+	var message = req.query.message;
+	console.log('received message: ' + message);
+});
+
+app.listen(5015);
+console.log("chat server is listening...");
